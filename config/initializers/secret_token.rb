d107e89325414bf426bfa622caa2963df6653d50
hirokiraj <hirokiraj@gmail.com>
2013-02-13 17:17:04 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StargazeRails::Application.config.secret_token = '0ba14f19ff6d527477b8b810e0f875c3b6c4f4027b6882c3f554d52783a9caae8bd2ef02cf3bb720694aa44b5b6755508eccbcb9539b349e0407256c66679764'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StargazeRails::Application.config.session_store :cookie_store, key: '_stargaze-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StargazeRails::Application.config.session_store :active_record_store
