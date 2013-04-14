0f04eed10300fd9edb27b4d9d0c31d32333932f8
hirokiraj <hirokiraj@gmail.com>
2013-04-14 13:28:38 +0200

    removed testing

--- Gemfile
+++ Gemfile
@@ -13,7 +13,6 @@ gem 'devise'
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'paperclip', "~> 3.0"
-gem 'ckeditor'
 
 group :development do
   gem 'better_errors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 StargazeRails::Application.routes.draw do
-  
-  mount Ckeditor::Engine => '/ckeditor'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
